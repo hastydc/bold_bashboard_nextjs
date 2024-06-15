8155f73c95668634a211d46ff1e680eb35d77de6
Efrain Mancilla <hastydc@gmail.com>
2024-06-14 22:51:02 -0500

    feat: 🎸 table mobile component

---
+++ src/_components/designSystem/table/table-desktop/TableDesktop.tsx
@@ -0,0 +1,13 @@
+import { TransactionData } from '@/models/transactionData.interface';
+
+type TableProps = TransactionData;
+
+const TableDesktop = (tableProps: TableProps) => {
+  return (
+    <>
+      <div>Desk</div>
+    </>
+  );
+};
+
+export default TableDesktop;
---
+++ src/_components/designSystem/table/table-mobile/transaction-card/hooks/useTransactionCard.tsx
@@ -0,0 +1,29 @@
+import { Transaction } from '@/models/transaction.interface';
+import { useEffect, useState } from 'react';
+import Style from '../../../table.module.scss';
+import { TransactionStatus } from '@/models/transactionStatus.enum';
+
+const useTransactionCard = (transaction: Transaction) => {
+  const [baseClassName, setBaseClassName] = useState('');
+  const [showMore, setShowMore] = useState(false);
+
+  const setClass = () => {
+    let baseClass = Style.td;
+
+    if (transaction.status === TransactionStatus.SUCCESS) {
+      baseClass += ` ${Style.tdBlue}`;
+    } else if (transaction.status === TransactionStatus.FAILED) {
+      baseClass += ` ${Style.tdGray}`;
+    }
+
+    setBaseClassName(baseClass);
+  };
+
+  useEffect(() => {
+    setClass();
+  }, [transaction]);
+
+  return { baseClassName, showMore, setShowMore };
+};
+
+export default useTransactionCard;
