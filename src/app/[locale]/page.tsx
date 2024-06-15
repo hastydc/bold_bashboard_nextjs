cda6c19dfe88a5f3c9eedd9dc8f62a4c90d76db6
Efrain Mancilla <hastydc@gmail.com>
2024-06-15 09:06:13 -0500

    style: 💄 remove unused import

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,6 @@ import Content from '@/_components/layout/content/Content';
 import Dashboard from '@/_components/dashboard/Dashboard';
 import { Suspense } from 'react';
 import DashboardSkeleton from '@/_components/skeletons/dashboard-skeleton/DashboardSkeleton';
-import dynamic from 'next/dynamic';
 
 const Home = () => {
   return (
