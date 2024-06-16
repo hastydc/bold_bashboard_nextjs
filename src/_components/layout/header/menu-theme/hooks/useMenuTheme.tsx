67fa83b32f3a31c36a30c62bb018ec601b6ac833
Efrain Mancilla <hastydc@gmail.com>
2024-06-15 19:40:03 -0500

    fix: 🐛 fix theme switch

--- src/_components/layout/header/menu-theme/hooks/useMenuTheme.tsx
+++ src/_components/layout/header/menu-theme/hooks/useMenuTheme.tsx
@@ -1,16 +1,21 @@
 import { Theme } from '@/_models/theme.enum';
 import { useTheme } from 'next-themes';
+import { useEffect } from 'react';
 
 const useMenuTheme = () => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
   const updateTheme = () => {
-    const value = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    const value = resolvedTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
 
     setTheme(value);
   };
 
-  return { theme, updateTheme };
+  useEffect(() => {
+    setTheme(Theme.LIGHT);
+  }, []);
+
+  return { resolvedTheme, updateTheme };
 };
 
 export default useMenuTheme;
